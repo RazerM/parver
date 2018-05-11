134a054a43d61e92dd4ddf8d823f243edb6c0d96
Frazer McLean <frazer@frazermclean.co.uk>
2018-05-11 18:42:39 +0200

    Test that un-normalised versions fail strict parse

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,21 +1,30 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function
 
-from hypothesis import given
+import pytest
+from hypothesis import assume, given
 
-from parver import Version
+from parver import ParseError, Version
 
 from .strategies import version_string, whitespace
 
 
 @given(whitespace, version_string(), whitespace)
-def test_parse_permissive_hypothesis(prefix, version, suffix):
+def test_parse_hypothesis(prefix, version, suffix):
     Version.parse(prefix + version + suffix)
 
 
 @given(whitespace, version_string(strict=True), whitespace)
-def test_parse_canonical_hypothesis(prefix, version, suffix):
+def test_parse_strict_hypothesis(prefix, version, suffix):
     Version.parse(prefix + version + suffix, strict=True)
+
+
+@given(version_string(strict=False))
+def test_parse_strict_error(version):
+    # Exclude already normalised versions
+    assume(str(Version.parse(version).normalize()) != version)
+    with pytest.raises(ParseError):
+        Version.parse(version, strict=True)
 
 
 @given(version_string())
