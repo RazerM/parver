535d30c70b1ad243067fe81b4530679e142134ce
Frazer McLean <frazer@frazermclean.co.uk>
2018-05-18 22:19:03 +0200

    Put public names under parver directly.

--- src/parver/__init__.py
+++ src/parver/__init__.py
@@ -5,3 +5,7 @@ from ._version import Version
 from ._parse import ParseError
 
 __all__ = ('Version', 'ParseError')
+
+from ._helpers import fixup_module_metadata
+fixup_module_metadata(__name__, globals())
+del fixup_module_metadata
--- src/parver/_helpers.py
+++ src/parver/_helpers.py
@@ -75,3 +75,17 @@ class NegativeInfinity(object):
 
 
 NegativeInfinity = NegativeInfinity()
+
+
+def fixup_module_metadata(module_name, namespace):
+    def fix_one(obj):
+        mod = getattr(obj, "__module__", None)
+        if mod is not None and mod.startswith("outcome."):
+            obj.__module__ = module_name
+            if isinstance(obj, type):
+                for attr_value in obj.__dict__.values():
+                    fix_one(attr_value)
+
+    for objname in namespace["__all__"]:
+        obj = namespace[objname]
+        fix_one(obj)
